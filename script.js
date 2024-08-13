fb3f95d73788cc1a6ca7e0c39b356e69b96e3ba1
swapna-m-d <160628142+swapna-m-d@users.noreply.github.com>
2024-08-13 22:09:27 +0530

    Added 3 files
    This is a hotel website that I learnt in an internship.

---
+++ script.js
@@ -0,0 +1,86 @@
+var x=5;
+/*
+if(x>20){
+    console.log("inside if block");
+}else{
+    console.log("inside else block");
+}*/
+/*
+if(x<=7){
+    if(x>2){
+console.log("inside nested if");
+    }else{
+        console.log("inside nested else");
+    }
+}
+else if(x<15){
+    console.log("inside else if block");
+}
+
+for(var x=0;x<=10;x++){
+    console.log(x=x+1); 
+}
+
+var x=prompt("Enter a number from 1-3");
+var y=parseInt(x);
+console.log(typeof y);
+
+switch (y) {
+    case 1:
+        console.log("1st case is executed");
+        break;
+    case 2:
+        console.log("2nd case is executed");
+        break;
+    case 3:
+        console.log("3rd case is executed");
+        break;
+    default:
+        console.log("Invalid");
+        break;
+}
+
+var name="swapna";
+console.log(name[5]);
+
+
+var arr=[100,200,400,600,800];
+arr[0]=0;
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.push();
+arr.shift(0);
+arr.unshift(3);
+
+var btn= document.getElementById("btn"); 
+btn.addEventListener("focus",color);
+function color(){
+    heading.style.color="red";
+}
+
+function add()
+{
+    return function ret(){
+        console.log("returned");
+        
+    };
+}
+var x=add();
+console.log(x);
+
+setTimeout(fuction(){
+    console.log("settimeout running");
+},  5000 );*/
+    
+let a= 10;
+console.log(a);
+{
+    let a=50;
+    console.log(a);
+}
+console.log(a);
+
+
+
+
